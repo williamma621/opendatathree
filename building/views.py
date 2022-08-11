39b7bc8f01f91018f119828aaa3c8f1357b509e1
Fuyi Yang <75815872+Fuyi-Yang@users.noreply.github.com>
2022-08-10 22:07:16 -0700

    update views.py

--- building/views.py
+++ building/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from .models import *
+from .buildings import buildings
 
+    
 # Create your views here.
 
 """
@@ -8,19 +11,28 @@ click_icon(): 用户点击见后会发生什么.展示info（我们还是用弹�
 	display_id(): 弹窗到一个新的界面，包括所有建筑名和ID。
 """
 
-def home(request):
+def index(request):
   #import data from static content(read from file), turn into classes from models
   #display the static content into html
-  return render(request, 'home.html', {'name': 'Jason'})
+  #longitude and lattitude
+
+  return render(request, 'index.html', {'buildings': get_buildings()})
+  # return render(request, 'home.html', {'name': 'Jason'})
 
 def click_icon(request):
   # 弹窗还是新界面？
-  pass
+  res = get_buildings()
+  
 
 def click_translate(request):
+  res = translate()
   #should we use the API or translate the text before hand and write into file?
-  pass
+  return render(request, 'index.html', {'buildings': res})
 
 def click_display_id(request):
+
   # one line of code rendering a new html file with /id
-  return render(request, 'id.html')
+
+  buildings.append()
+  
+  return render(request, 'id.html', {'buildings': buildings})
