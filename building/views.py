b060ab8fa3c17b4de5a5a662b660da90cc69f555
Fuyi Yang <75815872+Fuyi-Yang@users.noreply.github.com>
2022-09-05 21:07:28 -0700

    Update views.py

--- building/views.py
+++ building/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render
 from .models import *
 from .buildings import buildings
-
     
 # Create your views here.
 
@@ -15,14 +14,26 @@ def index(request):
   #import data from static content(read from file), turn into classes from models
   #display the static content into html
   #longitude and lattitude
-
-  return render(request, 'index.html', {'buildings': get_buildings()})
+  return render(request, 'index.html')
+  # return render(request, 'index.html', {'buildings': get_buildings()[:1]})
+  print("hello")
   # return render(request, 'home.html', {'name': 'Jason'})
+
+def map(request):
+
+  return render(request, "map.html")
 
 def click_icon(request):
   # 弹窗还是新界面？
-  res = get_buildings()
-  
+  # res = get_buildings()
+  # temp = request.GET['id']
+  # is_private = request.POST.get('is_private', False)
+  # print(temp)
+  text = request.GET.get('index')
+  print()
+  print(text)
+  print()
+  return render(request, 'id.html', {'buildings': get_buildings()[0]})
 
 def click_translate(request):
   res = translate()
