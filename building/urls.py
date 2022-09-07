4e710ee9f1a3d69ba2151650d506cf90970ac836
Fuyi Yang <75815872+Fuyi-Yang@users.noreply.github.com>
2022-09-06 23:16:38 -0700

    Update urls.py

--- building/urls.py
+++ building/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 
 from . import views
 
 urlpatterns = [
-  path('', views.index, name="index")
-
-]
+  path('', views.index, name="index"),
+  path('map/', views.map, name="map"),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
